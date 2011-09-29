f6e20f4c26ad08131421927fe0f6552ef92e8492
Subhash Chandran <subwiz@gmail.com>
2011-09-29 08:14:03 +0000

    name generation logic updated.

--- src/main/java/org/wiztools/jwget/FileNameGenerator.java
+++ src/main/java/org/wiztools/jwget/FileNameGenerator.java
@@ -1,28 +1,44 @@
 package org.wiztools.jwget;
 
 import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  *
  * @author subwiz
  */
 class FileNameGenerator {
-    static String getGenerated(URL url) {
-        final String urlFileName = url.getFile();
-        
-        if(urlFileName.equals("") || urlFileName.equals("/")) {
-            StringBuilder sb = new StringBuilder();
-            sb.append(url.getProtocol()).append("_");
-            sb.append(url.getHost());
-            if(url.getPort() != -1) {
-                sb.append("_")
-                        .append(url.getPort());
-            }
-            return sb.toString();
+    
+    private static String generateFileName(URL url) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(url.getProtocol()).append("_");
+        sb.append(url.getHost());
+        if(url.getPort() != -1) {
+            sb.append("_")
+                    .append(url.getPort());
         }
-        String str = urlFileName.replaceAll("/", "_");
+        sb.append(processSpecialChars(url.getFile()));
+        return sb.toString();
+    }
+    
+    private static String processSpecialChars(String str) {
+        str = str.replaceAll("/", "_");
         str = str.replaceAll("\\?", "_");
         str = str.replaceAll("=", "_");
         return str;
+    }
+    
+    static String getGenerated(URL url) {
+        final String urlFileName = url.getFile();
+        
+        Pattern p = Pattern.compile("^.*/([^/]+)$");
+        Matcher m = p.matcher(urlFileName);
+        if(m.matches()) {
+            final String name = m.group(1);
+            return processSpecialChars(name);
+        }
+        
+        return generateFileName(url);
     }
 }
